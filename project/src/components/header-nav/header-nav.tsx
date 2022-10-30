6142cfb857da6f99be9a0f3a10f6abb1a65748c6
Artem Kartavtsev <arkartavtsev@live.ru>
2022-10-30 18:53:28 +0300

    Компонент навигации шапки

---
+++ project/src/components/header-nav/header-nav.tsx
@@ -0,0 +1,43 @@
+type HeaderNavProps = {
+  isLogged: boolean;
+}
+
+
+const Header = (props: HeaderNavProps): JSX.Element => {
+  const { isLogged } = props;
+
+  return (
+    <nav className="header__nav">
+      <ul className="header__nav-list">
+        {
+          isLogged ? (
+            <>
+              <li className="header__nav-item user">
+                <div className="header__nav-profile">
+                  <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                  <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                </div>
+              </li>
+
+              <li className="header__nav-item">
+                <a className="header__nav-link" href="#">
+                  <span className="header__signout">Sign out</span>
+                </a>
+              </li>
+            </>
+          ) : (
+            <li className="header__nav-item user">
+              <a className="header__nav-link header__nav-link--profile" href="#">
+                <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+                <span className="header__login">Sign in</span>
+              </a>
+            </li>
+          )
+        }
+      </ul>
+    </nav>
+  );
+};
+
+
+export default Header;
--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -1,3 +1,6 @@
+import HeaderNav from '../header-nav/header-nav';
+
+
 type HeaderProps = {
   isLogged: boolean;
   hasNav?: boolean;
@@ -20,35 +23,9 @@ const Header = (props: HeaderProps): JSX.Element => {
 
           {
             hasNav ? (
-              <nav className="header__nav">
-                <ul className="header__nav-list">
-                  {
-                    isLogged ? (
-                      <>
-                        <li className="header__nav-item user">
-                          <div className="header__nav-profile">
-                            <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                            <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                          </div>
-                        </li>
-
-                        <li className="header__nav-item">
-                          <a className="header__nav-link" href="#">
-                            <span className="header__signout">Sign out</span>
-                          </a>
-                        </li>
-                      </>
-                    ) : (
-                      <li className="header__nav-item user">
-                        <a className="header__nav-link header__nav-link--profile" href="#">
-                          <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                          <span className="header__login">Sign in</span>
-                        </a>
-                      </li>
-                    )
-                  }
-                </ul>
-              </nav>
+              <HeaderNav
+                isLogged= { isLogged }
+              />
             ) : ''
           }
         </div>
