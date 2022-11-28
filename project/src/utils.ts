d94dbe3e4f721f02dd8b200917c78f275c71f2f1
Artem Kartavtsev <arkartavtsev@live.ru>
2022-11-29 02:39:40 +0300

    Исправление вывода отзывов

--- project/src/utils.ts
+++ project/src/utils.ts
@@ -7,5 +7,6 @@ import { SortingOption } from './types/sorting-option';
 export const pluralize = (number: number, caption: string) => `${ number } ${ caption }${ number !== 1 ? 's' : '' }`;
 
 export const getFormattedDate = (ISODate: string, format: string) => DateTime.fromISO(ISODate).toFormat(format);
+export const getDateMilliseconds = (ISODate: string) => DateTime.fromISO(ISODate).toMillis();
 
 export const getSortingOptionByType = (type: string) => sortingOptions.find((option) => option.type === type) as SortingOption;
