652773f7186f2dc10e9b9a96c2e2cb521be2f706
Artem Kartavtsev <arkartavtsev@live.ru>
2022-12-06 15:21:57 +0300

    Перенос стилей лоадера в отдельный файл

--- project/src/components/loader/loader.tsx
+++ project/src/components/loader/loader.tsx
@@ -1,3 +1,5 @@
+import './loader.css';
+
 import { useAppSelector } from '../../hooks/use-app-selector';
 
 import { getIsOffersDataLoading } from '../../store/offers-process/selectors';
@@ -8,17 +10,9 @@ const Loader = (): JSX.Element => {
 
   return (
     <div
+      className='loader'
       style={{
-        position: 'fixed',
-        zIndex: '2000',
-        top: '0',
-        left: '0',
-        display: isDataLoading ? 'grid' : 'none',
-        placeItems: 'center',
-        height: '100%',
-        width: '100%',
-        fontSize: '40px',
-        backgroundColor: '#f5f5f5'
+        display: !isDataLoading ? 'none' : ''
       }}
     >
       <p>Loading...</p>
